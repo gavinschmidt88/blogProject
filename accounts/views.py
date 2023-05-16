25ccfe4f3c49bd80b9de27729613eb24e47f2038
Gavin Scott Schmidt <gschmidt7@ivytech.edu>
2023-05-16 10:39:25 -0400

    Added a 'reset password' link on homepage

--- accounts/views.py
+++ accounts/views.py
@@ -3,8 +3,16 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 
-
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+""" def profileView(request, username):
+    return render(
+            request,
+            "accounts/profile.html",
+            {
+                "error_message": "You are not logged in!.",
+            },
+        ) """
